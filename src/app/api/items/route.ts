267f58527a443a3075573293afd03b3324385cb4
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-06-13 18:09:30 -0300

    feat: 🗑️ delete api

--- src/app/api/items/route.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "@/lib/prisma";
-import { NextResponse, NextRequest } from "next/server";
-
-export async function GET(request: Request) {
-  const items = await prisma.item.findMany();
-
-  return items;
-}
